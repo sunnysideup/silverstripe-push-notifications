2632e528f0b7b6b2befd05443f702e4c02c8c6ea
Nathan Cox <me@nathan.net.nz>
2024-05-24 16:39:22 +1200

    Add temporary fetch event to service worker

--- client/dist/javascript/service-worker.js
+++ client/dist/javascript/service-worker.js
@@ -16,26 +16,7 @@ self.addEventListener('push', function (event) {
   event.waitUntil(self.registration.showNotification(title, options))
 });
 
-self.addEventListener("pushXZ", (event) => {
-  if (!(self.Notification && self.Notification.permission === "granted")) {
-    return;
-  }
-
-  const data = event.data?.json() ?? {};
-  const title = data.title || "Something Has Happened";
-  const message =
-    data.message || "Here's something you might want to check out.";
-  const icon = "images/new-notification.png";
-
-  const notification = new self.Notification(title, {
-    body: message,
-    tag: "simple-push-demo-notification",
-    icon,
-  });
-
-  notification.addEventListener("click", () => {
-    clients.openWindow(
-      "https://example.blog.com/2015/03/04/something-new.html",
-    );
-  });
+self.addEventListener("fetch", (event) => {
+  return fetch(event.request);
 });
+
