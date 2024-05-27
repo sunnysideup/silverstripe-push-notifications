3897f334a3f900ff72af5bde232d837a85178a41
Nathan Cox <me@nathan.net.nz>
2024-05-27 15:48:57 +1200

    Service worker tweaks

--- client/dist/javascript/service-worker.js
+++ client/dist/javascript/service-worker.js
@@ -1,22 +1,32 @@
-self.addEventListener('push', function (event) {
+self.addEventListener("fetch", (event) => {
+  return fetch(event.request);
+});
 
-  const data = event.data?.json() ?? {};
-  const title = data.title || "Fallback title";
-  const body = data.body || "Fallback message";
+self.addEventListener('install', function (event) {
+  console.log('installed');
+});
+
+self.addEventListener('push', function (event) {
+  const obj = event.data.json();
+  
+  const title = obj.title || "Fallback title";
+  const body = obj.body || "Fallback message";
+
 
  // const title = 'Push Timeline Update 1'
 
   const options = {
     body: body,
-    icon: 'images/icon.png',
-    badge: 'images/badge.png',
+    icon: 'https://push.rakau.com/512.png',
+    badge: 'https://push.rakau.com/512.png',
     action: 'open',
   }
   
   event.waitUntil(self.registration.showNotification(title, options))
 });
 
-self.addEventListener("fetch", (event) => {
-  return fetch(event.request);
-});
 
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close()
+  event.waitUntil(clients.openWindow('https://push.rakau.com'))
+});
