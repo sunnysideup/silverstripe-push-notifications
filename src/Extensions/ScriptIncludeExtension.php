a8709ef0dfb44ffe67b3b8b7b88e255124be78c3
SDGM <sion98madden@gmail.com>
2024-06-18 15:01:12 +1200

    Include public key in requirements

--- src/Extensions/ScriptIncludeExtension.php
+++ src/Extensions/ScriptIncludeExtension.php
@@ -2,6 +2,7 @@
 
 namespace Sunnysideup\PushNotifications\Extensions;
 
+use SilverStripe\Core\Environment;
 use SilverStripe\Core\Extension;
 use SilverStripe\View\Requirements;
 
@@ -12,6 +13,12 @@ class ScriptIncludeExtension extends Extension
 {
     public function onAfterInit()
     {
+        $key = Environment::getEnv('SS_VAPID_PUBLIC_KEY');
+
         Requirements::javascript('/_resources/vendor/sunnysideup/push-notifications/client/dist/javascript/service-worker-start.js');
+        Requirements::customScript(<<<JS
+            let vapid_public_key="$key";
+        JS
+        );
     }
 }
