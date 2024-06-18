4c77e6b3e9c53813660dc277daf7e5c2a6842545
SDGM <sion98madden@gmail.com>
2024-06-18 15:47:02 +1200

    Hide subscribe buttons when disabled

--- src/Extensions/ScriptIncludeExtension.php
+++ src/Extensions/ScriptIncludeExtension.php
@@ -16,6 +16,7 @@ class ScriptIncludeExtension extends Extension
         $key = Environment::getEnv('SS_VAPID_PUBLIC_KEY');
 
         Requirements::javascript('/_resources/vendor/sunnysideup/push-notifications/client/dist/javascript/service-worker-start.js');
+        Requirements::css('/_resources/vendor/sunnysideup/push-notifications/client/dist/css/push.css');
         Requirements::customScript(<<<JS
             let vapid_public_key="$key";
         JS
