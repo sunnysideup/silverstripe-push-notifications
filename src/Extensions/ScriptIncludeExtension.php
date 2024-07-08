ee263899f86d02cabb8b635de1d110a1aa0f9ec3
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-07-08 16:37:03 +1200

    FIX: exception for one signal

--- src/Extensions/ScriptIncludeExtension.php
+++ src/Extensions/ScriptIncludeExtension.php
@@ -13,11 +13,14 @@ class ScriptIncludeExtension extends Extension
 {
     public function onAfterInit()
     {
+        if($this->owner->dataRecord->UseOneSignal) {
+            return;
+        }
         $key = Environment::getEnv('SS_VAPID_PUBLIC_KEY');
-
         Requirements::javascript('/_resources/vendor/sunnysideup/push-notifications/client/dist/javascript/service-worker-start.js');
         Requirements::css('/_resources/vendor/sunnysideup/push-notifications/client/dist/css/push.css');
-        Requirements::customScript(<<<JS
+        Requirements::customScript(
+            <<<JS
             let vapid_public_key="$key";
         JS
         );
